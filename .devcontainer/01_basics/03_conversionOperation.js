8051764c7b46f5a653db3d54139b48827a11a4d0
0preetam1 <152862025+0preetam1@users.noreply.github.com>
2024-05-05 10:33:47 +0000

    conversion Of data types

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "Preetam"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+//"33" => 33
+//"33abc" => NaN i.e Not a Number
+//true => 1 ; false => 0;
+
+let isLoggedIn = "Preetam"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+//1 => true; 0 => false
+//"" => false
+//"Preetam" => true
+
+let someNumber = 22
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+   
+console.log(typeof stringNumber);
